450d9e6b2af1a05a8f46aa0c4ed354a211cc33b0
shakthi1995 <77444165+shakthi1995@users.noreply.github.com>
2021-01-14 18:33:33 +0530

    Updated the project
    Added the base branch to the repository

---
+++ SeleniumCSharpProject/SeleniumCSharpProject/CustomControl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpProject
+{
+    public class CustomControl 
+    {
+       
+    }
+}
---
+++ SeleniumCSharpProject/SeleniumCSharpProject/DriverHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpProject
+{
+    public class DriverHelper
+    {
+        public static IWebDriver Driver  { get; set; }
+    }
+}
